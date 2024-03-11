7a827d6735531a4552af3383553b3e3ea5995174
Dani-vn18 <juanda.vn18@gmail.com>
2024-03-11 17:40:26 -0500

    Prueba nro no recuerdo

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'django_crud_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL')),
 }
 
 
